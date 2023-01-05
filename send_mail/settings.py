39a8c5ba8070b0eb41be98628f2b6e725ac221df
Shankar-1212 <21btrcl024@jainuniversity.ac.in>
2023-01-06 00:05:07 +0530

    minor changes

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -15,20 +15,15 @@ SECRET_KEY = 'django-insecure-%9fwo&^+!w(bzbpt%0xcfjkg9#jcxbtui6r_r@bxe0kx+4(zkz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','localhost', '127.0.0.1']
 
 DEFAULT_FROM_EMAIL = 'sssbb5838@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST = env('EMAIL_HOST')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
